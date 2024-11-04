edf9a20b84e2257789b41f65bfb269ee536247a6
Jade Alvares <jadea@webother.com>
2024-11-04 11:47:12 +0000

    has more than 8 characters

--- passwordValidation.js
+++ passwordValidation.js
@@ -1,7 +1,9 @@
 const isPasswordValid = (input) => {
-  return true;
+  const chars = input.split('');
+  const minNumChars = 8;
+  return chars.length > minNumChars;
 }
 
-const input = 'def';
+const input = 'abcdefghijklmno';
 
 export { isPasswordValid };
--- test/passwordValidation.test.js
+++ test/passwordValidation.test.js
@@ -2,6 +2,10 @@ import { isPasswordValid } from '../passwordValidation';
 
 describe('is password valid', () => {
   it('has an input', () => {
-    expect(isPasswordValid('abc')).toBe(true);
+    expect(isPasswordValid('abc')).toBe(false);
+  });
+
+  it('has more than 8 characters', () => {
+    expect(isPasswordValid('abcdefghijklmno')).toBe(true);
   })
 })
